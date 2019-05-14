1360c045afbf6f1260bd729b6d32a19c383f409d
J.Repka <jessrepka@gmail.com>
2019-05-14 17:15:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 minikube start;
 minikube addons enable default-storageclass;
 helm init;
+sleep 30;
 kubectl create ns jenkins;
 helm install --name jenkins stable/jenkins --namespace jenkins;
 minikube dashboard
